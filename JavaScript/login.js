f32603c7d6334bb729421dd7714290c1b2781631
AlexLaMattina <alexlama@udel.edu>
2021-10-05 15:47:30 -0400

    updated login html, css, bootstrap, and js

---
+++ JavaScript/login.js
@@ -0,0 +1,29 @@
+$('.form').find('input').on('keyup', function (e) {
+  
+    var $this = $(this),
+        label = $this.prev('label');
+  
+        if (e.type === 'keyup') {
+              if ($this.val() === '') {
+            label.removeClass('active highlight');
+          } else {
+            label.addClass('active highlight');
+          }
+        }
+  
+});
+  
+$('.tab a').on('click', function (e) {
+    
+    e.preventDefault();
+    
+    $(this).parent().addClass('active');
+    $(this).parent().siblings().removeClass('active');
+    
+    target = $(this).attr('href');
+  
+    $('.tab-content > div').not(target).hide();
+    
+    $(target).fadeIn(300);
+    
+  });
