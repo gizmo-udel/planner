17579d6c64f854d22d5b0d79615f44e47b36f38f
Ryan Allarey <60989959+rallarey@users.noreply.github.com>
2021-11-16 10:40:58 -0500

    logging users in
    changed calendar button to go to month view and login function in auth.js

--- JavaScript/auth.js
+++ JavaScript/auth.js
@@ -21,4 +21,23 @@ signupForm.addEventListener('submit', (e) => {
   });
 });
 
+// login 
+
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  
+  // get user info
+  const email = loginForm['login-email'].value;
+  const password = loginForm['login-password'].value;
+
+  // log the user in
+  auth.signInWithEmailAndPassword(email, password).then((cred) => {
+    console.log(cred.user);
+    //reset form
+    loginForm.reset();
+  });
+
+});
+
 */
